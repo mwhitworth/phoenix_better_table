bba61fa9ccf24b3a878473a16a6f7095704d6fb9
Matthew Whitworth <matt@tallarium.com>
2023-11-22 10:24:28 +0000

    bump minimum phoenix_live_view to reflect TagEngine change

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule PhoenixBetterTable.MixProject do
       {:live_isolated_component, "~> 0.7.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:phoenix_live_view, "~> 0.18"}
+      {:phoenix_live_view, "~> 0.19"}
     ]
   end
 
