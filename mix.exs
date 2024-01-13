c35a26089f192cd96870e667c8ec8cdd22e81fe3
Matt Whitworth <mwhitworth@gmail.com>
2024-01-13 11:43:22 +0000

    0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixBetterTable.MixProject do
   def project do
     [
       app: :phoenix_better_table,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
