ac154611833dbe8120c99a15e30921610753bf1a
Matt Whitworth <mwhitworth@gmail.com>
2024-01-16 15:26:12 +0000

    up to LV 0.18.0 now supported

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule PhoenixBetterTable.MixProject do
       {:live_isolated_component, "~> 0.8.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:phoenix_live_view, ">= 0.18.17"}
+      {:phoenix_live_view, ">= 0.18.0"}
     ]
   end
 
